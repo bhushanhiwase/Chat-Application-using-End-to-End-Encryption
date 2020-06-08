d6066b91c7a87836fa03fdbd8d43f336ae43d4ed
Bhushan Hiwase <bhushiwase@gmail.com>
2020-06-08 00:16:33 -0700

    Update CLIENT.py

--- CLIENT.py
+++ CLIENT.py
@@ -14,11 +14,11 @@ flag = False
 # check and create RSA key pairs here.
 
 try:
-    if os.stat("/home/ubuntu/.ssh/B_publickey.pem").st_size > 0:
-       print("RSA key pairs already exist")
+    if os.stat("/home/ubuntu/.ssh/B_publickey.pem").st_size > 0:        # checks if key is aready present i.e. pem file has key data
+       print("RSA key pairs already exist")                             # If size greater than 0 bytes then file exist
        pass
     else:
-       key = RSA.generate(1024)
+       key = RSA.generate(1024)                                          # Generates key if it doesn't exist
        f = open("/home/ubuntu/.ssh/B_privatekey.pem", "wb")
        f.write(key.exportKey('PEM'))
        f.close()
@@ -27,7 +27,7 @@ try:
        f = open("/home/ubuntu/.ssh/B_publickey.pem", "wb")
        f.write(pubkey.exportKey('OpenSSH'))
        f.close()
-except OSError:
+except OSError:                                                            # If there is no pem file then generate keys
     print("No file found, creating a new one....")
     flag = True
     key = RSA.generate(1024)
@@ -55,11 +55,11 @@ file.close()
 #Function for encryption
 def encrypt(msg):
     '''
-    Function to Encrypt the message and to get the ciphertext
+    Function to Encrypt the message with public key and to get the ciphertext
     '''
     f = open("/home/ubuntu/.ssh/A_publickey.pem", "rb")
-    key = RSA.importKey(f.read())
-    x = key.encrypt(bytes(msg, "utf-8"), 32)
+    key = RSA.importKey(f.read())                                   
+    x = key.encrypt(bytes(msg, "utf-8"), 32)                        # Encryption done using public key          
     return x[0]                           # we took this because we get a tuple 
 
 #Function for decryption
@@ -70,7 +70,7 @@ def decryptt(msg):
     f1 = open("/home/ubuntu/.ssh/B_privatekey.pem", "rb")
     key1 = RSA.importKey(f1.read())
     z = key1.decrypt(msg) # try direct string
-    return z.decode() 
+    return z.decode()           # Decode the bytes data into string format, can use str(z, "utf-8")
 
 def doagain():
     '''
@@ -103,11 +103,17 @@ def sendmsg():
 
 #Function used to receive messages
 def recvmsg():
+    '''
+   Function for reciving messages from the user
+   '''
     recived = s.recv(2048)
     string = recived
     output = str(string)[1:]
 
     if decryptt(string) == "exit":
+        '''
+   Function for decrypting cypher text to get back orignal message
+   '''
         print("A left the chatroom...")
         sys.exit()
     elif len(string) > 0:
